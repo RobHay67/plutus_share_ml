8de565cbd223735481f6163869345d65972fa85a
Rob Hay <rob@blueswoop.com>
2019-06-29 20:16:05 +1000

    reconfigure date and volume modules - add an OHLC data controller

--- controller.py
+++ controller.py
@@ -1,9 +1,21 @@
 import pandas as pd
 
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Local Modules
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------
+from features_dates                 import add_day_of_the_week_features, add_month_of_the_year_features
+from features_volume                import add_average_volume, volume_values
+
 import app
-import dates
+# import dates
 import price
-import volume
+# import volume
+
+
+
+
 
 pd.set_option('display.max_columns', None)
 
@@ -14,8 +26,8 @@ list_of_share_codes = app.list_of_share_codes( share_df )
 share_df = app.add_sequential_counter( share_df )
 
 # ----------------------------------------------- Attach Volume Features to the dataset
-share_df = volume.add_average_volume( share_df )
-share_df = volume.volume_values( share_df )
+share_df = add_average_volume( share_df )
+share_df = volume_values( share_df )
 # ----------------------------------------------- Attach Price  Features to the dataset
 share_df = price.price_values( share_df )
 
@@ -27,8 +39,8 @@ share_df = price.price_values( share_df )
 
 
 # ----------------------------------------------- Attach Date Features to the dataset
-share_df = dates.add_day_of_the_week_features( share_df )
-share_df = dates.add_month_of_the_year_features( share_df )
+share_df = add_day_of_the_week_features( share_df )
+share_df = add_month_of_the_year_features( share_df )
 
 print ( share_df.tail(7) )
 
