3b5078f229cb9d33f258c69cc1028a61074e82df
Rob Hay <rob@blueswoop.com>
2019-06-29 20:11:38 +1000

    add application logging file

---
+++ application_log.py
@@ -0,0 +1,8 @@
+
+
+
+
+def print_seperator( seperator_type ):
+    if   seperator_type=='key'      : print ( '=' * 120 )
+    elif seperator_type == 'single' : print ( '-' * 120 )
+    else                            : print ( '*' * 120 )
